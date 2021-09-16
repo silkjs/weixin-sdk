39fea116d85b7fb8759eb50fad147e79c4d233a9
dasoncheng <dasoncheng@outlook.com>
2021-09-16 15:26:42 +0800

    feat: update api

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 /**
  * 打招呼
- * @param name 姓名
+ * @param name 对方姓名
+ * @param from 己方姓名
  * @returns 招呼话术
  */
-export function SayHello(name: string): string {
-  return `Hi, ${name}! I'm your new neighbor. `;
+export function SayHello(name: string, from: string): string {
+  return `Hi, ${name}! I'm your new neighbor, my name is ${from}`;
 }
