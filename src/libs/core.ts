7369d4c66e9558fac2c6b0348a0b04782c12e778
dasoncheng <dasoncheng@outlook.com>
2021-09-18 18:16:41 +0800

    feat: 回调机制更新

--- src/libs/core.ts
+++ src/libs/core.ts
@@ -1,8 +1,8 @@
 import { md5 } from "../utils/forge.js";
 import { Apis, SignatureFn, Tags } from "../models/weixin.model.js";
 interface CommandActions<T> {
-  success: (res: any) => T; // 接口调用成功时执行的回调函数。
-  fail: (err: any) => Error; // 接口调用失败时执行的回调函数。
+  success?: (res: any) => T; // 接口调用成功时执行的回调函数。
+  fail?: (err: any) => Error; // 接口调用失败时执行的回调函数。
   complete?: () => void; // 接口调用完成时执行的回调函数，无论成功或失败都会执行。
   cancel?: () => void; // 用户点击取消时的回调函数，仅部分有用户取消操作的api才会用到。
   trigger?: () => void; // 监听Menu中的按钮点击时触发的方法，该方法仅支持Menu中的相关接口。
@@ -52,24 +52,21 @@ export class WeiXinSdkCore {
    */
   command<T = void>(
     operation: Apis,
-    actions: CommandActions<T>,
-    options: CommandOptions = {}
+    options: CommandOptions = {},
+    actions: CommandActions<T> = {}
   ): Promise<T> {
     return new Promise<T>((resolve, reject) => {
       this.config()
         .then(() => {
           this._wx[operation](
-            Object.assign(
-              {
-                success: (res: any) => {
-                  resolve(actions.success(res));
-                },
-                fail: (err: any) => {
-                  reject(actions.fail(err));
-                },
+            Object.assign(options, {
+              success: (res: any) => {
+                resolve(actions.success?.(res) ?? res);
               },
-              options
-            )
+              fail: (err: any) => {
+                reject(actions.fail?.(err) ?? err);
+              },
+            })
           );
         })
         .catch((err) => {
