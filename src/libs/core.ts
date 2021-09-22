b7902bea0553c06a902360b603a8333bdff8366e
dasoncheng <dasoncheng@outlook.com>
2021-09-22 10:02:21 +0800

    feat: 开放主动初始化接口

--- src/libs/core.ts
+++ src/libs/core.ts
@@ -28,53 +28,9 @@ export class WeiXinSdkCore {
     this.openTagList = options.openTagList ?? [];
   }
   /**
-   * 判断当前客户端版本是否支持指定JS接口
+   * 主动初始化
    */
-  checkJsApi(apis: Apis[]): Promise<void> {
-    return new Promise<void>((resolve, reject) => {
-      this._wx.checkJsApi({
-        jsApiList: apis, // 需要检测的JS接口列表，所有JS接口列表见附录2,
-        success: (res: any) => {
-          // 以键值对的形式返回，可用的api值true，不可用为false
-          // 如：{"checkResult":{"chooseImage":true},"errMsg":"checkJsApi:ok"}
-          resolve(res);
-        },
-        fail: () => {
-          reject();
-        },
-      });
-    });
-  }
-  /**
-   * 微信SDK通用接口调用
-   * @param action 接口名称
-   * @param options 接口参数
-   */
-  command<T = any>(
-    operation: Apis,
-    options: CommandOptions = {},
-    actions: CommandActions<T> = {}
-  ): Promise<T> {
-    return new Promise<T>((resolve, reject) => {
-      this.config()
-        .then(() => {
-          this._wx[operation](
-            Object.assign(options, {
-              success: (res: any) => {
-                resolve(actions.success?.(res) ?? res);
-              },
-              fail: (err: any) => {
-                reject(actions.fail?.(err) ?? err);
-              },
-            })
-          );
-        })
-        .catch((err) => {
-          reject(err);
-        });
-    });
-  }
-  private config(): Promise<void> {
+  init(): Promise<void> {
     return new Promise<void>((resolve, reject) => {
       const url = `${location.origin}${location.pathname}${location.search}`;
       const sign = md5(url);
@@ -103,6 +59,53 @@ export class WeiXinSdkCore {
             reject(err);
           });
       }
+    });
+  }
+  /**
+   * 判断当前客户端版本是否支持指定JS接口
+   */
+  checkJsApi(apis: Apis[]): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+      this._wx.checkJsApi({
+        jsApiList: apis, // 需要检测的JS接口列表，所有JS接口列表见附录2,
+        success: (res: any) => {
+          // 以键值对的形式返回，可用的api值true，不可用为false
+          // 如：{"checkResult":{"chooseImage":true},"errMsg":"checkJsApi:ok"}
+          resolve(res);
+        },
+        fail: () => {
+          reject();
+        },
+      });
+    });
+  }
+  /**
+   * 微信SDK通用接口调用
+   * @param action 接口名称
+   * @param options 接口参数
+   */
+  command<T = any>(
+    operation: Apis,
+    options: CommandOptions = {},
+    actions: CommandActions<T> = {}
+  ): Promise<T> {
+    return new Promise<T>((resolve, reject) => {
+      this.init()
+        .then(() => {
+          this._wx[operation](
+            Object.assign(options, {
+              success: (res: any) => {
+                resolve(actions.success?.(res) ?? res);
+              },
+              fail: (err: any) => {
+                reject(actions.fail?.(err) ?? err);
+              },
+            })
+          );
+        })
+        .catch((err) => {
+          reject(err);
+        });
     });
   }
 }
